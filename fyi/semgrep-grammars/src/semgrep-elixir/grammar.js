ec16ad0fb224a64eaaafe2b2fc4a92da7be36be4
Amarin Phaosawasdi <amchiclet@users.noreply.github.com>
2024-01-11 16:18:19 -0800

    Release files generated by ocaml-tree-sitter c2c0ef6.

--- fyi/semgrep-grammars/src/semgrep-elixir/grammar.js
+++ fyi/semgrep-grammars/src/semgrep-elixir/grammar.js
@@ -19,13 +19,13 @@ module.exports = grammar(base_grammar, {
   rules: {
     // No need for _SEMGREP_EXPRESSION hack here, because Elixir allows
     // toplevel expressions.
-      
+
     // Metavariables
     identifier: ($, previous) => {
-	return prec(1, choice(
+      return choice(
         previous,
         $._semgrep_metavariable
-	));
+      );
     },
 
     _semgrep_metavariable: $ => token(/\$[A-Z_][A-Z_0-9]*/),
@@ -33,22 +33,31 @@ module.exports = grammar(base_grammar, {
     // Ellipsis
     // No need for extensions to _expressions for ellipsis because
     // Elixir already uses "..." as valid identifiers
-      
+    //
+    // However, we do need to override the "pair" rule which is used
+    // for keyword parameters, arguments, and map items. Otherwise,
+    // ellipsis won't work in places where Elixir expects
+    // keyword/pairs, such as
+    //   foo(some_arg: 0, ...)
+    //   %{some_item: 0, ...}
+    // Also note that now there is ambiguity whether foo(...) is an
+    // identity or pair, we set the pair rule to have a lower
+    // precedence
+    pair: ($, previous) => {
+      return prec(-1, choice(
+        previous,
+        '...',
+      ));
+    },
+
     _expression: ($, previous) => choice(
       ...previous.members,
       $.deep_ellipsis,
     ),
-      
+
     // The actual ellipsis rules
     deep_ellipsis: $ => seq(
             '<...', $._expression, '...>'
     ),
-
-      pair: ($, previous) => {
-	  return choice(
-	      previous,
-	      '...',
-	  );
-      },
   }
 });
